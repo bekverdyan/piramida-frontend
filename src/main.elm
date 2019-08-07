e35b2e4ded7567a7cb06af42563312d45cf05ef6
Aram Bekverdyan <aram.bekverdyan@gmail.com>
2019-08-08 01:29:57 +0400

    fit: draft of agent creation and list view

--- src/main.elm
+++ src/main.elm
@@ -1,68 +1,129 @@
 module Main exposing (Model, Msg(..), init, main, update, view)
 
 import Browser
-import Html exposing (Html, button, input, div, span, text)
+import Html exposing (Html, button, div, input, li, span, text, ul)
 import Html.Attributes exposing (..)
-import Html.Events exposing (onClick)
+import Html.Events exposing (onClick, onInput)
+
 
 main =
-  Browser.sandbox { init = init, update = update, view = view }
+    Browser.sandbox { init = init, update = update, view = view }
+
 
 
 -- MODEL
 
+
 type alias Model =
-  { agents : List (Maybe Agent)
-  , createdAgent : Maybe Agent
-  , agentName : String
-  , agentLevel : String
-  , brokerId : String
-  }
+    { agents : List Agent
+    , createdAgent : Agent
+    , agentName : String
+    , agentLevel : String
+    , brokerId : String
+    }
+
 
 init : Model
 init =
-  Model [] Nothing "" "" ""
+    Model [] { id = "", name = "", level = "", brokerId = "" } "" "" ""
+
 
 type alias Agent =
-  { id : String
-  , name : String
-  , level : String
-  , brokerId : String
-  }
+    { id : String
+    , name : String
+    , level : String
+    , brokerId : String
+    }
+
+
 
 -- UPDATE
 
-type Msg =
-  CreateAgent
+
+type Msg
+    = CreateAgent
+    | NameInput String
+    | LevelInput String
+    | BrokerIDInput String
+
 
 update : Msg -> Model -> Model
 update msg model =
     case msg of
-      CreateAgent ->
-        { model
-        | createdAgent = Just (createAgent model.agentName model.agentLevel model.brokerId)
-        , agents = model.createdAgent :: model.agents }
+        NameInput name ->
+            { model
+                | agentName = name
+            }
+
+        LevelInput level ->
+            { model
+                | agentLevel = level
+            }
+
+        BrokerIDInput brokerId ->
+            { model
+                | brokerId = brokerId
+            }
+
+        CreateAgent ->
+            { model
+                | agents = createAgent model.agentName model.agentLevel model.brokerId :: model.agents
+            }
+
 
 createAgent : String -> String -> String -> Agent
 createAgent name level brokerId =
-  { id = "gago"
-  , name = name
-  , level = level
-  , brokerId = brokerId
-  }
+    { id = "gago"
+    , name = name
+    , level = level
+    , brokerId = brokerId
+    }
+
+
 
 -- VIEW
+
+
 view : Model -> Html Msg
 view model =
-  div [ style "display" "table"]
-  [ div [ style "display" "table-row"]
-    [ div [ style "display" "table-cell"] [text "fafo"]
-    , div [ style "display" "table-cell"]
-      [ div [] [input [placeholder "Agent name", value model.agentName] []]
-      , div [] [input [placeholder "Agent level", value model.agentLevel] []]
-      , div [] [input [placeholder "Broker ID", value model.brokerId] []]
-      , button [onClick CreateAgent] [text "Create"]
-      ]
-    ]
-  ]
+    div [ style "display" "table" ]
+        [ div [ style "display" "table-row" ]
+            [ div [ style "display" "table-cell" ]
+                [ ul [] (toHtmlFunction model.agents) ]
+            , div [ style "display" "table-cell" ]
+                [ div []
+                    [ input
+                        [ placeholder "Agent name"
+                        , value model.agentName
+                        , onInput NameInput
+                        ]
+                        []
+                    ]
+                , div []
+                    [ input
+                        [ placeholder "Agent level"
+                        , value model.agentLevel
+                        ]
+                        []
+                    ]
+                , div []
+                    [ input
+                        [ placeholder "Broker ID"
+                        , value model.brokerId
+                        ]
+                        []
+                    ]
+                , button [ onClick CreateAgent ] [ text "Create" ]
+                ]
+            ]
+        ]
 
+
+toHtmlFunction : List Agent -> List (Html Msg)
+toHtmlFunction agents =
+    List.map myHtmlF agents
+
+
+myHtmlF : Agent -> Html Msg
+myHtmlF agent =
+    li [] [ text agent.name ]
